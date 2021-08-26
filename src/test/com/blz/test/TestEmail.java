24db6db56661bbe20dce8222cbaebcc9b96bfa8b
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-26 17:52:37 +0530

    Refactor: Email Valid check with all samples

--- src/test/com/blz/test/TestEmail.java
+++ src/test/com/blz/test/TestEmail.java
@@ -1,6 +1,10 @@
 package com.blz.test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 import com.blz.userdetails_valid.UserRegistrationValid;
 
@@ -16,25 +20,33 @@ public class TestEmail {
 	public static void main(String[] args) {
 		UserRegistrationValid userRegistrationValid = new UserRegistrationValid();
 		Scanner sc = new Scanner(System.in);
-		int key = 0;
-		while (key != 2) {
-			try {
-				System.out.println("Select From Option\n1.Enter Email ID\n2.exit");
-				key = sc.nextInt();
-				switch (key) {
-				case 1:
-					System.out.println("Enter Your Email");
-					String userEmail = sc.next();
-					validTest(userRegistrationValid, userEmail);
-					break;
-				default:
-					break;
-				}
-			}catch (Exception e) {
-				System.out.println(e);
-				key=2;
-			}
+		List<String> emailIds = Arrays.asList(
+			"abc@yahoo.com",
+			"abc-100@yahoo.com",
+			"abc-100@yahoo.com",
+			"abc111@abc.com",
+			"abc-100@abc.net",
+			"abc.100@abc.com.au",
+			"abc@1.com",
+			"abc@gmail.com.com",
+			"abc+100@gmail.com",
+			
+			"abc",
+			"abc@.com.my",
+			"abc123@gmail.a",
+			"abc123@.com",
+			"abc123@.com.com",
+			".abc@abc.com",
+			"abc()*@gmail.com",
+			"abc@%*.com",
+			"abc..2002@gmail.com",
+			"abc.@gmail.com",
+			"abc@abc@gmail.com",
+			"abc@gmail.com.1a",
+			"abc@gmail.com.aa.au");
+		
+		for (String userEmail : emailIds) {
+			validTest(userRegistrationValid,userEmail);	
 		}
-
 	}
 }
