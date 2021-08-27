c728b8d04faae429f9250b845dd700d5423a0ea6
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-27 15:11:50 +0530

    Refactor: Test Multiple Entry Email

--- src/test/com/blz/using_junit/EmailTest.java
+++ src/test/com/blz/using_junit/EmailTest.java
@@ -2,21 +2,68 @@ package com.blz.using_junit;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import com.blz.userdetails_valid.UserRegistrationValid;
 
+@RunWith(Parameterized.class)
 public class EmailTest {
 
-	UserRegistrationValid userRegistrationValid = new UserRegistrationValid();
-	@Test
-	public void testEmailValid_success() {
-		assertTrue(userRegistrationValid.EmailID("ydeepak141097@gmail.com"));
+	private String userEmail;
+	private boolean expectedResult;
+	UserRegistrationValid userRegistrationValid;
+	
+	public EmailTest(String userEmail, boolean expectedResult) {
+		this.userEmail = userEmail;
+		this.expectedResult = expectedResult;
+		
 	}
 	
-	@Test()
-	public void testEmailValid_fail() {
-		assertFalse(userRegistrationValid.EmailID("@gamilydeepak14109.com"));
+	@Parameterized.Parameters
+	public static Collection data() {
+		
+		return Arrays.asList(new Object[][] {
+			{"abc@yahoo.com",true},
+			{"abc-100@yahoo.com",true},
+			{"abc-100@yahoo.com",true},
+			{"abc111@abc.com",true},
+			{"abc-100@abc.net",true},
+			{"abc.100@abc.com.au",true},
+			{"abc@1.com",true},
+			{"abc@gmail.com.com",true},
+			{"abc+100@gmail.com",true},
+			
+			{"abc",false},
+			{"abc@.com.my",false},
+			{"abc123@gmail.a",false},
+			{"abc123@.com",false},
+			{"abc123@.com.com",false},
+			{".abc@abc.com",false},
+			{"abc()*@gmail.com",false},
+			{"abc@%*.com",false},
+			{"abc..2002@gmail.com",false},
+			{"abc.@gmail.com",false},
+			{"abc@abc@gmail.com",false},
+			{"abc@gmail.com.1a",false},
+			{"abc@gmail.com.aa.au",false}});
+	}
+	
+	@Before
+	public void initialize() {
+		userRegistrationValid = new UserRegistrationValid();
+	}
+	
+	@Test
+	public void testEmailValid_success() {
+		boolean actual = userRegistrationValid.EmailID(userEmail);
+		boolean expected = expectedResult;
+		assertEquals(expected, actual);
 	}
 
 }
