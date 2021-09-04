c9a8cd88fef32c2678ac66041ff8edd2790bf76a
DYSTAR14 <ydeepak141097@gmail.com>
2021-09-04 16:42:39 +0530

    Refactor: Validate using Lambda Function

--- src/main/com/blz/userdetails_valid/UserRegistrationValid.java
+++ src/main/com/blz/userdetails_valid/UserRegistrationValid.java
@@ -1,28 +1,31 @@
 package com.blz.userdetails_valid;
 
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import com.blz.Constants;
 import com.blz.exception.InvalidInputException;
 
+interface IsValidate{
+	public boolean IsValidPattern(String input, String regexPattern);
+}
 
 public class UserRegistrationValid {
-	public boolean IsValidPattern(String input, String regexPattern) {
+	
+	IsValidate check = (String input, String regexPattern) -> {
 		Pattern pattern = Pattern.compile(regexPattern);
 		Matcher matcher = pattern.matcher(input);
-		return matcher.matches();
-	}
+		return matcher.matches();};
 	
 	public boolean firstName(String userFirstName) throws InvalidInputException {
-		if(IsValidPattern(userFirstName, Constants.FIRST_NAME_REGEX))
+		if(check.IsValidPattern(userFirstName, Constants.FIRST_NAME_REGEX))
 			return true;
 		else
 			throw new InvalidInputException(Constants.INVALID_FIRST_NAME);
 	}
 	
 	public boolean lastName(String userLastName) throws InvalidInputException {
-		if(IsValidPattern(userLastName, Constants.LAST_NAME_REGEX))
+		if(check.IsValidPattern(userLastName, Constants.LAST_NAME_REGEX))
 			return true;
 		else
 			throw new InvalidInputException(Constants.INVALID_LAST_NAME);
@@ -30,21 +33,21 @@ public class UserRegistrationValid {
 	
 	public boolean EmailID(String userEmail) throws InvalidInputException {
 //		return IsValidPattern(userEmail, Constants.EMAIL_ID_REGEX);
-		if(IsValidPattern(userEmail, Constants.EMAIL_ID_REGEX))
+		if(check.IsValidPattern(userEmail, Constants.EMAIL_ID_REGEX))
 			return true;
 		else
 			throw new InvalidInputException(Constants.INVALID_EMAIL_ID);
 	}
 
 	public boolean MobileNumber(String userMobile) throws InvalidInputException {
-		if(IsValidPattern(userMobile, Constants.MOBILE_NUMBER_REGEX))
+		if(check.IsValidPattern(userMobile, Constants.MOBILE_NUMBER_REGEX))
 			return true;
 		else
 			throw new InvalidInputException(Constants.INVALID_MOBILE_NUMBER);
 	}
 	
 	public boolean Password(String userPass) throws InvalidInputException {
-		if(IsValidPattern(userPass, Constants.PASSWORD_REGEX))
+		if(check.IsValidPattern(userPass, Constants.PASSWORD_REGEX))
 			return true;
 		else
 			throw new InvalidInputException(Constants.INVALID_PASSWORD);
